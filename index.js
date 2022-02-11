00467cb7c1ea9897325eecb82bab8eefdfc53a01
Kawana T Small <jazzytech@Kawanas-MacBook-Pro.local>
2022-02-10 20:07:56 -0500

    pushing my code

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+
+  function logShout(string) {
+    console.log ("HELLO");
+  }
+
+  function logWhisper(string) {
+    console.log ("hello");
+  }
+
+  function sayHiToHeadphonedRoommate(string){
+      
+      console.log("I can\'t hear you!");
+      if (string);
+    return string.toLowerCase;
+
+  }
+  
+    
+  
+  
+
